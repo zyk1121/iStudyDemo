3ce1044d87a4375878352914f8b5f7d2d179e54c
张元科 <zhangyuanke@leador.com.cn>
2016-03-27 13:40:55 +0800

    通讯录

--- iStudyDemo/iStudyDemo/Classes/Modules/System/SystemFunctionViewController.m
+++ iStudyDemo/iStudyDemo/Classes/Modules/System/SystemFunctionViewController.m
@@ -11,12 +11,106 @@
 #import "ReactiveCocoa/ReactiveCocoa.h"
 #import "masonry.h"
 #import "UIKitMacros.h"
+#import "AddressBookViewController.h"
+
+@interface SystemFunctionViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *listData;
+@property (nonatomic, strong) NSMutableArray *listViewControllers;
+
+@end
 
 @implementation SystemFunctionViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    [self setupData];
+    [self setupUI];
+    [self.view setNeedsUpdateConstraints];
 }
+
+#pragma mark - masonry
+
+- (void)updateViewConstraints
+{
+    [super updateViewConstraints];
+    
+    [self.tableView mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+}
+
+#pragma mark - private method
+
+- (void)setupUI
+{
+    _tableView = ({
+        UITableView *tableview = [[UITableView alloc] init];
+        tableview.delegate = self;
+        tableview.dataSource = self;
+        tableview.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+        tableview;
+    });
+    [self.view addSubview:_tableView];
+}
+
+- (void)setupData
+{
+    _listData = [[NSMutableArray alloc] init];
+    _listViewControllers = [[NSMutableArray alloc] init];
+    
+    // 1.通讯录
+    [_listData addObject:@"通讯录"];
+    AddressBookViewController *addressController = [[AddressBookViewController alloc] init];
+    [_listViewControllers addObject:addressController];
+    // 2.
+    
+}
+
+
+#pragma mark - UITableViewDelegate & UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return  self.listData.count;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    UIViewController *vc = [self.listViewControllers objectAtIndex:indexPath.row];
+    vc.title = [self.listData objectAtIndex:indexPath.row];
+   [self.navigationController pushViewController:vc animated:YES];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *reuseIdetify = @"tableViewCellIdetify";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseIdetify];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuseIdetify];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        cell.showsReorderControl = YES;
+    }
+    
+    cell.textLabel.backgroundColor = [UIColor clearColor];
+    cell.textLabel.text = [self.listData objectAtIndex:indexPath.row];
+    //    cell.detailTextLabel.text = [self.listData objectForKey:[self.listData allKeys][indexPath.row]];
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 60;
+}
+
 
 @end
